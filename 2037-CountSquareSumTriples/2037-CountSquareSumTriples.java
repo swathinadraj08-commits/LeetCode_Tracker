a48f0d91c559c01f58635410ddfa71591c890559
Swathi N <swathinadraj08@gmail.com>
2026-07-14 14:21:12 +0530

    Create 2037-CountSquareSumTriples

---
+++ 2037-CountSquareSumTriples/2037-CountSquareSumTriples.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:21:11 PM
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+        for(int a = 1; a <= n; a++){
+            for(int b = 1; b <= n; b++){
+                for(int c = 1; c <= n; c++){
+                    if(a*a + b*b == c*c){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
