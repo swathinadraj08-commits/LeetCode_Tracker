adcb05d6957dbf3026a3943e497bddb77140e30e
Swathi N <swathinadraj08@gmail.com>
2026-07-14 14:33:53 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,18 @@
+// Last updated: 7/14/2026, 2:33:53 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false; 
+            }
+            left++;
+            right--;
+        }
+        return true; // It's a palindrome
+    }
+}
