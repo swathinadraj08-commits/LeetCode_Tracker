9ab3df25ae4386b82858783c41c7c35ac9d7d615
Swathi N <swathinadraj08@gmail.com>
2026-07-14 14:34:17 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 2:34:16 PM
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);   // add bit
+            carry = sum / 2;      // update carry
+        }
+
+        return sb.reverse().toString();
+    }
+}
